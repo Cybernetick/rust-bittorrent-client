849d408691972de9ec5c5a90be421f597b5d2361
ivankhulup <vkhulup@gmail.com>
2024-01-23 17:09:24 +0200

    stage 3. parsing lists

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
 use serde_json;
-use std::env;
+use std::{env, vec};
 use serde_json::Number;
 
 // Available if you need it!
 // use serde_bencode
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    let delimiter = encoded_value.find(':');
-    return match delimiter {
-        Some(delimited_safe) => {
-            let key = encoded_value[..delimited_safe].parse::<usize>().expect("unable to parse key as digit");
-            let value = &encoded_value[delimited_safe + 1..=delimited_safe + key];
-            serde_json::Value::String(String::from(value))
+fn decode_bencoded_string(encoded_string: &str) -> (serde_json::Value, usize) {
+    return match encoded_string.chars().nth(0).expect("fail to create iterator over input string") {
+        'i' => {
+            let delimiter = encoded_string.find('e').expect("supplied string starts like integer, but missing enclosing symbol");
+            let parsed_number = encoded_string[1..delimiter].parse::<i64>().expect("unable to parse integer");
+            (serde_json::Value::Number(Number::from(parsed_number)), delimiter + 1)
         }
-        None => {
-            if encoded_value.starts_with('i') {
-                let delimiter = encoded_value.find('e').expect("supplied string starts like integer, but missing enclosing symbol");
-                let parsed_number = encoded_value[1..delimiter].parse::<i64>().expect("unable to parse integer");
-                serde_json::Value::Number(Number::from(parsed_number))
-            } else {
-                panic!("unknown input {}", encoded_value)
+        item if item.is_digit(10) => {
+            let delimiter = encoded_string.find(':');
+            match delimiter {
+                None => {
+                    panic!("malformed input. expecting a string, but cannot find a semicolon delimiter for {}", encoded_string)
+                }
+                Some(delimiter_safe) => {
+                    let key = encoded_string[..delimiter_safe].parse::<usize>().expect("unable to parse key as digit");
+                    let value = &encoded_string[delimiter_safe + 1..=delimiter_safe + key];
+                    (serde_json::Value::String(String::from(value)), (delimiter_safe + value.len() + 1))
+                }
             }
+        }
+        'l' => {
+            let mut total: Vec<serde_json::Value> = vec![];
+            let mut cursor: usize = 1;
+            while encoded_string.chars().nth(cursor).unwrap() != 'e' {
+                let (result, size) = decode_bencoded_string(&encoded_string[cursor..encoded_string.len()]);
+                total.push(result);
+                cursor += size;
+            }
+
+            (serde_json::Value::Array(total), cursor + 1)
+        }
+        'e' => {
+            (serde_json::Value::Null, 0)
+        }
+        _ => {
+            panic!("unknown input {}", encoded_string)
         }
     };
 }
@@ -30,16 +49,18 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        decode_bencoded_value("7:sevenoo");
-    } else {
-        let command = &args[1];
+        let response = decode_bencoded_string("l5:helloi52ee");
+        println!("{}", response.0);
+        return;
+    }
 
-        if command == "decode" {
-            let encoded_value = &args[2];
-            let decoded_value = decode_bencoded_value(encoded_value);
-            println!("{}", decoded_value.to_string());
-        } else {
-            println!("unknown command: {}", args[1])
-        }
+    let command = &args[1];
+
+    if command == "decode" {
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_string(encoded_value);
+        println!("{}", decoded_value.0.to_string());
+    } else {
+        println!("unknown command: {}", args[1])
     }
 }
